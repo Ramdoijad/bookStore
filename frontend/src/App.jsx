0d9da4039e177005db5720d4fe2c56c1e825a6fd
muskan <musakanmansuri493@gmail.com>
2024-07-22 10:00:21 -0700

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import Banner from "./Components/Banner";
+import Navbar from "./Components/Navbar";
+
+export default function App() {
+  return (
+    <h1 className="">
+    <Navbar/>
+    <Banner/>
+    </h1>
+  )
+}
---
+++ frontend/src/Components/Banner.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+  <>
+  <div className='max-w-screen-2xl container flex flex-col md:flex-row mx-auto md:px-20'>
+  <div className=' w-full md:w-1/2'>Left</div>
+  <div className=' w-full md:w-1/2'> Right</div>
+  </div>
+  </>
+  )
+}
+
+export default Banner
